5d9e53a9aadbd4c7da3ed8eb351984add529926d
KamiiilPawlak <kami.patryk.pawlak@gmail.com>
2025-12-02 23:49:59 +0100

    dalte Field

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator, Field
+from pydantic import BaseModel, field_validator
 from datetime import datetime
 from typing import Generic, TypeVar
 from random import randint

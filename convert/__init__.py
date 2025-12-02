ca497a257d436b2dbcd5cb8abd8d8e8b56915c13
KamiiilPawlak <kami.patryk.pawlak@gmail.com>
2025-12-02 23:49:18 +0100

    add validator fun

--- convert/__init__.py
+++
@@ -1,5 +0,0 @@
-from .distance import cm_to_m, m_to_cm 
-
-
-
-__all__ = ["cm_to_m", "m_to_cm"]
--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+from pydantic import BaseModel, field_validator, Field
+from datetime import datetime
 from typing import Generic, TypeVar
 from random import randint
+import re
+
+
+class Event(BaseModel):
+    name: str
+    data_start: datetime
+
+    @field_validator("name")
+    def validate_name(cls, v: str) -> str:
+        if not re.fullmatch(r"[A-Za-z1-9]{2,30}", v):
+            raise ValueError(
+                "Name must be 2-30 characters long and contain only A-Z, a-z, 1-9"
+            )
+        return v
+
+    @field_validator('data_start')
+    def event_must_later_than_today(cls, value):
+        if value < datetime.today():
+            raise ValueError("Date must be later than today")
+        
+        return value
 
 Number = TypeVar("Number", int, float)
 T = Generic[TypeVar("T")]
@@ -10,3 +33,4 @@ def add(a: Number, b: Number) -> Number:
 print(add(randint(0, 10), randint(10, 100)))
 
 print(type(Number))
+print(datetime.today())
